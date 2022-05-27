7a08d19e70f47ee740538a1c86a5e6e3b191940c
rmatovu987 <rmatovu987@gmail.com>
2022-05-27 10:32:50 +0300

    Pomodoro app

--- pomodoro/main.py
+++ pomodoro/main.py
@@ -16,15 +16,30 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
+
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title('Pomodoro')
 window.config(padx=100, pady=50, bg=YELLOW)
 
-canvas = Canvas(width=1000, height=1120, bg=YELLOW, highlightthickness=0)
+window.after(1000,)
+
+title = Label(text='Timer', fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50, 'bold'))
+title.grid(column=1, row=0)
+
+canvas = Canvas(width=400, height=446, bg=YELLOW, highlightthickness=0)
 tomato = PhotoImage(file='tomato.png')
-canvas.create_image(500, 560, image=tomato)
-canvas.create_text(500, 610, text='00:00', fill='white', font=(FONT_NAME, 35, 'bold'))
-canvas.pack()
+canvas.create_image(200, 223, image=tomato)
+canvas.create_text(200, 265, text='00:00', fill='white', font=(FONT_NAME, 35, 'bold'))
+canvas.grid(column=1, row=1)
+
+start = Button(text='Start', highlightthickness=0)
+start.grid(column=0, row=2)
+
+reset = Button(text='Reset', highlightthickness=0)
+reset.grid(column=2, row=2)
+
+checks = Label(text='✔', fg=GREEN, bg=YELLOW)
+checks.grid(column=1, row=3)
 
 window.mainloop()
